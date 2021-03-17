7ccad5eb317b3569f4531499bc72fe564570cc54
YagneshReddy <60852590+YagneshReddy@users.noreply.github.com>
2021-03-18 01:47:23 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,104 @@
+# from array import *
+# arr = array('i',[])
+# p=int(input('size=?'))
+# for i in range(p):
+#        b=int(input('next value='))
+#        arr.append(b)
+# n=int(input('n='))
+# m=int(input('m='))
+# sum=0
+# stars=0
+# for j in range(p-(m-1)):
+#        sum=0
+#        for e in range(m):
+#                sum=sum + arr[j+e]
+#        if n==sum:
+#               stars+=1
+# print(stars)
+
+
+# from numpy import *
+# m1= matrix('1 2 3;6 5 4;1 2 3')
+# m2= matrix('2 3 4;5 6 7;8 7 0')
+# m3=m1+m2
+# print(m1*m2)
+
+# def sum(a,b):
+#    print(a+b)
+#    return(a+b)
+
+# sum(67,73)
+# print(sum(7656,725758))
+
+# def count(ls):
+#        even,odd=0,0
+#        for i in ls:
+#          if i%2==0:
+#            even+=1
+#          else:
+#            odd+=1
+#        return even,odd
+   
+# lst=[]
+# for i in range(5):
+#    lst.append(int(input('num=')))
+# even,odd = count(lst)
+# print ('even:',even ,'odd:',odd)
+#print("even : {} and odd : {}".format(even,odd))
+# def fib(n):
+#        a,b=0,1
+#        print(a)
+#        print(b)
+#        for i in range(2,n):
+#          c=a+b
+#          a=b
+#          b=c
+#          print(c)
+
+# x=int(input('num='))
+# fib(x)
+
+# def fac(n):
+#        c=1
+#        for i in range(1,n+1):
+#               c=c*i
+#        return c
+
+# print(fac(int(input('num='))))
+# ABOVE HALLELUJAAH!!!!!!!
+
+# def fac(n):
+#       if n==0:
+#              return 1
+#       return n*fac(n-1)
+
+# print(fac(int(input('num='))))
+
+# fa = lambda a,b:a*b
+
+# print(fa(int(input('a=')),int(  input('b='))))
+
+#LEARN DECORATERS
+#print("waresgfch"+__name__)
+
+# a=8
+# print(type(a))
+
+# class student:
+#     def __init__(self,rno,age):
+#         self.rolno=rno
+#         self.age=age
+
+#     def just(self):
+#         print('details='+str(self.rolno),self.age)
+
+
+
+# s1=student(1801086,23)
+# s2=student(1801088,20)
+
+# s1.just()
+# s2.just()
+
+#print(int(input("first number=")) + int(input("second number=")))
+print(2-7-16/12)
